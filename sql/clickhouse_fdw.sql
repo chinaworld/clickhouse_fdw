2762296972283b4c7aebd410322d46f7f7878f78
Andrey Borodin <amborodin@acm.org>
2017-08-14 09:58:44 +0500

    ping pong

--- sql/clickhouse_fdw.sql
+++ sql/clickhouse_fdw.sql
@@ -27,3 +27,9 @@ LANGUAGE C STRICT;
 CREATE FOREIGN DATA WRAPPER clickhouse_fdw
   HANDLER clickhouse_fdw_handler
   VALIDATOR clickhouse_fdw_validator;
+
+CREATE OR REPLACE FUNCTION retcomposite(IN integer, IN integer,
+    OUT f1 integer, OUT f2 integer, OUT f3 integer)
+    RETURNS SETOF record
+    AS 'filename', 'retcomposite'
+    LANGUAGE C IMMUTABLE STRICT;
