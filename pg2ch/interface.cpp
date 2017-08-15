974977abb1c57a6e05a5cb0c6e11fa23a5f3e0b6
Andrey Borodin <amborodin@acm.org>
2017-08-15 10:01:03 +0500

    ping pong

--- pg2ch/interface.cpp
+++ pg2ch/interface.cpp
@@ -1528,14 +1528,11 @@ extern "C" int read_ch_query(CHReadCtx *ctx){
 
     for (size_t j = 0; j < ctx->natts; ++j)
         {
-            //auto & col = blcs[ctx->currentBlock].getByPosition(j);
-
+            auto & col = blcs[ctx->currentBlock].getByPosition(j);
 
             ctx->tupleValues[j] = out_buf.position();
-            //(*col.type.get()).serializeTextEscaped(*col.column.get(), ctx->currentRow, out_buf);
+            (*col.type.get()).serializeTextEscaped(*col.column.get(), ctx->currentRow, out_buf);
 
-            *out_buf.position() = '0';
-            out_buf.position()++;
             *out_buf.position() = 0;
             out_buf.position()++;
         }
