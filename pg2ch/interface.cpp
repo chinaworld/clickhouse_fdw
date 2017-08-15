0a74db74a4f8abbf35968bb9c739f8fccb363d5f
Andrey Borodin <amborodin@acm.org>
2017-08-15 10:43:46 +0500

    ping pong

--- pg2ch/interface.cpp
+++ pg2ch/interface.cpp
@@ -1499,6 +1499,8 @@ extern "C" void begin_ch_query(CHReadCtx *ctx){
 }
 
 extern "C" void end_ch_query(CHReadCtx *ctx){
+
+    std::cout<<"end call"<< ctx->currentRow <<std::endl;
     auto blcs = (std::vector<DB::Block>*)ctx->blocks;
     delete blcs;
 }
